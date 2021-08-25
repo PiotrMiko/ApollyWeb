3c0e00878da688287b79ae821404f52930bbdb48
PiotrMiko <piotrmik90@gmail.com>
2021-08-25 20:35:50 +0200

    update

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const menu = document.querySelector("#menu-toggle");
+const hambMenu = document.querySelector(".hamb-menu");
+const cross = document.querySelector(".close-cross");
+const menuopen = document.querySelector(".top__bar__nav");
+const navMenu = document.querySelector(".nav__menu");
+const active = document.querySelector(".active-menu");
 
 const toggle = () => {
-  console.log("click");
-  menu.classList.toggle("menu-active");
-  menu.classList.toggle("top__bar__nav");
+  if (menu.classList == "top__bar__nav") {
+    console.log("click");
+    hambMenu.style.display = "none";
+    cross.style.display = "flex";
+    menu.classList.toggle("menu-active");
+    menu.classList.toggle("top__bar__nav");
+  } else {
+    cross.style.display = "none";
+    hambMenu.style.display = "flex";
+    menu.classList.toggle("menu-active");
+    menu.classList.toggle("top__bar__nav");
+  }
 };
 
 const toggleBtn = document
