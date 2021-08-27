7b110fc5125bf94d60fef492f773cac762c99e31
PiotrMiko <piotrmik90@gmail.com>
2021-08-27 16:46:07 +0200

    menu update

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-const nav = document.querySelector(".top__bar__nav");
+const nav = document.getElementById("menu-toggle");
 const hambIcon = document.querySelector(".hamb-menu");
 const closeIcon = document.querySelector(".close-cross");
 
 const toggle = () => {
-  if ((hambIcon.style.display = "flex")) {
-    hambIcon.style.display = "none";
-    closeIcon.style.display = "flex";
-    nav.style.left = "0px";
-  } else if ((closeIcon.style.left = "flex")) {
-    closeIcon.style.display = "none";
-    hambIcon.style.display = "flex";
-    nav.style.left = "100%";
-    console.log("ok2");
+  if (nav.classList == "top__bar__nav") {
+    nav.classList.toggle("top__bar__nav");
+    nav.classList.toggle("top__bar__nav_active");
+    hambIcon.src = "./img/Icons/close-line.svg";
+    console.log("flex");
+  } else if (nav.classList == "top__bar__nav_active") {
+    nav.classList.toggle("top__bar__nav_active");
+    nav.classList.toggle("top__bar__nav");
+    hambIcon.src = "./img/Icons/align-justify.svg";
+    console.log("none");
   }
 };
 
 const toggleBtn = document
   .querySelector(".top__bar__menu-toggle")
   .addEventListener("click", toggle);
+//  <img class="close-cross" src="./img/Icons/close-line.svg" alt="" />
