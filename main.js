43f6e983c2378cf13a4e7e3a4c96aa939527f222
PiotrMiko <piotrmik90@gmail.com>
2021-09-02 23:33:27 +0200

    update

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 const nav = document.getElementById("menu-toggle");
 const hambIcon = document.querySelector(".hamb-menu");
 const closeIcon = document.querySelector(".close-cross");
+let modes = ([] = document.querySelectorAll(".item-ani"));
 
-// const resizeCapture = () => {
-
+// const toggle = () => {
+//   if (nav.classList == "top__bar__nav") {
+//     nav.classList.toggle("top__bar__nav");
+//     nav.classList.toggle("top__bar__nav_active");
+//     hambIcon.src = "./img/Icons/close-line.svg";
+//   } else if (nav.classList == "top__bar__nav_active") {
+//     nav.classList.toggle("top__bar__nav_active");
+//     nav.classList.toggle("top__bar__nav");
+//     hambIcon.src = "./img/Icons/align-justify.svg";
+//   }
 // };
+
+// const toggleBtn = document
+//   .querySelector(".top__bar__menu-toggle")
+//   .addEventListener("click", toggle);
 const toggle = () => {
   if (nav.classList == "top__bar__nav") {
     nav.classList.toggle("top__bar__nav");
@@ -16,18 +29,16 @@ const toggle = () => {
     hambIcon.src = "./img/Icons/align-justify.svg";
   }
 };
+let interval = 1000 / 2;
+modes.forEach((i, index) => {
+  setTimeout(() => {
+    setTimeout(() => {
+      modes[index].style.opacity = "1";
+    }, index * interval);
+  }, 1000 / 2);
+});
+//// napisz to teraz sam
 
 const toggleBtn = document
   .querySelector(".top__bar__menu-toggle")
   .addEventListener("click", toggle);
-
-let modes = ([] = document.querySelectorAll(".item-ani"));
-let interval = 1000; //one second
-modes.forEach((mode, index) => {
-  setTimeout(() => {
-    setTimeout(() => {
-      modes[index].style.opacity = "1";
-      console.log(mode);
-    }, index * interval);
-  }, 1000 / 2);
-});
